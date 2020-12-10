9aa03208bd9a73584366c03a3922e4bd4ef2bb88
Daniel Fergisz <daniel.fergisz@gmail.com>
2020-12-10 18:20:15 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Formularz przesłania metodę dispose, aby wyczyścić listę składników.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wymagane przez Projektanta formularzy systemu Windows
+    Private components As System.ComponentModel.IContainer
+
+    'UWAGA: następująca procedura jest wymagana przez Projektanta formularzy systemu Windows
+    'Możesz to modyfikować, używając Projektanta formularzy systemu Windows. 
+    'Nie należy modyfikować za pomocą edytora kodu.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.R2 = New System.Windows.Forms.RadioButton()
+        Me.R1 = New System.Windows.Forms.RadioButton()
+        Me.D_OS = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Disks = New System.Windows.Forms.ComboBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Log1 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(405, 227)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.GroupBox1)
+        Me.TabPage1.Controls.Add(Me.Button2)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.Disks)
+        Me.TabPage1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(397, 198)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "PS4"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.R2)
+        Me.GroupBox1.Controls.Add(Me.R1)
+        Me.GroupBox1.Controls.Add(Me.D_OS)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(21, 6)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(361, 92)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        '
+        'R2
+        '
+        Me.R2.AutoSize = True
+        Me.R2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.R2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.R2.Location = New System.Drawing.Point(185, 58)
+        Me.R2.Name = "R2"
+        Me.R2.Size = New System.Drawing.Size(81, 21)
+        Me.R2.TabIndex = 6
+        Me.R2.Text = "Update"
+        Me.R2.UseVisualStyleBackColor = True
+        '
+        'R1
+        '
+        Me.R1.AutoSize = True
+        Me.R1.Checked = True
+        Me.R1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.R1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.R1.Location = New System.Drawing.Point(111, 58)
+        Me.R1.Name = "R1"
+        Me.R1.Size = New System.Drawing.Size(55, 21)
+        Me.R1.TabIndex = 5
+        Me.R1.TabStop = True
+        Me.R1.Text = "Full"
+        Me.R1.UseVisualStyleBackColor = True
+        '
+        'D_OS
+        '
+        Me.D_OS.Location = New System.Drawing.Point(194, 21)
+        Me.D_OS.Name = "D_OS"
+        Me.D_OS.Size = New System.Drawing.Size(131, 31)
+        Me.D_OS.TabIndex = 1
+        Me.D_OS.Text = "Download"
+        Me.D_OS.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 28)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(130, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Firmware file v8.03:"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(33, 151)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(335, 32)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Prepare"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 120)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 17)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Select  Drive:"
+        '
+        'Disks
+        '
+        Me.Disks.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Disks.FormattingEnabled = True
+        Me.Disks.Location = New System.Drawing.Point(132, 117)
+        Me.Disks.Name = "Disks"
+        Me.Disks.Size = New System.Drawing.Size(250, 24)
+        Me.Disks.TabIndex = 2
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(397, 198)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "PS3"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Log1
+        '
+        Me.Log1.Enabled = False
+        Me.Log1.Location = New System.Drawing.Point(16, 245)
+        Me.Log1.Name = "Log1"
+        Me.Log1.ReadOnly = True
+        Me.Log1.Size = New System.Drawing.Size(397, 22)
+        Me.Log1.TabIndex = 1
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(16, 276)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(397, 23)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 2000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(429, 311)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Log1)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "PS_OS v1.00"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Disks As ComboBox
+    Friend WithEvents D_OS As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Log1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents R2 As RadioButton
+    Friend WithEvents R1 As RadioButton
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,130 @@
+﻿Imports System.IO
+Imports System.ComponentModel
+Imports System.Net
+
+Public Class Form1
+    Dim appPath As String = IO.Path.Combine(Application.StartupPath, "")
+
+    Private Sub D_OS_Click(sender As Object, e As EventArgs) Handles D_OS.Click
+        Log1.Text = "Downloading file, please wait.." ' pobieranie pliku
+        ProgressBar1.Style = ProgressBarStyle.Marquee
+        Dim wClient As New WebClient()
+        AddHandler wClient.DownloadFileCompleted, AddressOf OnDownloadComplete
+        If R1.Checked = True Then
+            wClient.DownloadFileAsync(New System.Uri("http://dus01.ps4.update.playstation.net/update/ps4/image/2020_1130/rec_3e5241162736abd81a14e5922093c5c0/PS4UPDATE.PUP"), appPath + "\PS4\FULL\PS4UPDATE.PUP")
+        End If
+        If R2.Checked = True Then
+            wClient.DownloadFileAsync(New System.Uri("http://dus01.ps4.update.playstation.net/update/ps4/image/2020_1130/sys_a261a9388c591adae9ac010c0b73483b/PS4UPDATE.PUP"), appPath + "\PS4\UPDATE\PS4UPDATE.PUP")
+        End If
+        wClient.Dispose()
+    End Sub
+    Private Sub OnDownloadComplete(ByVal sender As Object, ByVal e As AsyncCompletedEventArgs)
+        ProgressBar1.Style = ProgressBarStyle.Blocks
+        If Not e.Cancelled AndAlso e.Error Is Nothing Then  'po pobraniu pliku
+            Log1.Text = "Download success"
+        Else
+            Log1.Text = "Download Fail !!"
+        End If
+    End Sub
+    Private Sub Disks_DropDown(sender As Object, e As EventArgs) Handles Disks.DropDown
+        Disks.Items.Clear()
+
+        For Each drive As IO.DriveInfo In IO.DriveInfo.GetDrives()
+
+            ' Detection drive type
+            Dim drive_type As String = ""
+            If drive.DriveType = DriveType.Fixed Then
+                drive_type = "Local Disk"
+            ElseIf drive.DriveType = DriveType.CDRom Then
+                drive_type = "CD-Rom drive"
+            ElseIf drive.DriveType = DriveType.Network Then
+                drive_type = "Network drive"
+            ElseIf drive.DriveType = DriveType.Removable Then
+                drive_type = "Removable Disk"
+            ElseIf drive.DriveType = DriveType.Unknown Then
+                drive_type = "Unknown"
+            End If
+
+            ' The drive name and its type is added to the list of drives
+            Me.Disks.Items.Add(drive.Name & " [" & drive_type & "]")
+        Next
+
+        ' It selects the first item in the list (ComboBox)
+        ' Disks.SelectedIndex = 0
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Directory.Exists("PS4\FULL\") = True Then
+        Else
+            Directory.CreateDirectory("PS4\FULL\")
+        End If
+        If Directory.Exists("PS4\UPDATE\") = True Then
+        Else
+            Directory.CreateDirectory("PS4\UPDATE\")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Disks.Text.Length = 0 Then
+            Log1.Text = "Please select drive first !!"
+        Else
+            ProgressBar1.Style = ProgressBarStyle.Marquee
+            If System.IO.Directory.Exists(Mid(Disks.Text, 1, 3) & "PS4\UPDATE") Then
+            Else
+                Log1.Text = "Creating directories: PS4\UPDATE.."
+                System.IO.Directory.CreateDirectory(Mid(Disks.Text, 1, 3) & "PS4\UPDATE")
+                If System.IO.Directory.Exists(Mid(Disks.Text, 1, 3) & "PS4\UPDATE") Then
+                    Log1.Text = "Creating directories: PS4\UPDATE.. OK"
+                Else
+                    Log1.Text = "FAIL"
+                End If
+            End If
+
+            If R1.Checked = True Then
+                If File.Exists("PS4\FULL\PS4UPDATE.PUP") Then
+                    FileCopy("PS4\FULL\PS4UPDATE.PUP", Mid(Disks.Text, 1, 3) & "PS4\UPDATE\PS4UPDATE.PUP")
+                    If File.Exists(Mid(Disks.Text, 1, 3) & "PS4\UPDATE\PS4UPDATE.PUP") Then
+                        Log1.Text = "Done."
+                    Else
+                        Log1.Text = "Fail !!"
+                    End If
+                Else
+                    Log1.Text = "Please Download Firmware First !!"
+                End If
+            End If
+
+            If R2.Checked = True Then
+                If File.Exists("PS4\UPDATE\PS4UPDATE.PUP") Then
+                    FileCopy("PS4\UPDATE\PS4UPDATE.PUP", Mid(Disks.Text, 1, 3) & "PS4\UPDATE\PS4UPDATE.PUP")
+                    If File.Exists(Mid(Disks.Text, 1, 3) & "PS4\UPDATE\PS4UPDATE.PUP") Then
+                        Log1.Text = "Done."
+                    Else
+                        Log1.Text = "Fail !!"
+                    End If
+                Else
+                    Log1.Text = "Please Download Firmware First !!"
+                End If
+
+            End If
+        End If
+        ProgressBar1.Style = ProgressBarStyle.Blocks
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If File.Exists("PS4\FULL\PS4UPDATE.PUP") Then
+            R1.ForeColor = Color.DarkGreen
+        Else
+            R1.ForeColor = Color.Black
+        End If
+
+        If File.Exists("PS4\UPDATE\PS4UPDATE.PUP") Then
+            R2.ForeColor = Color.DarkGreen
+        Else
+            R2.ForeColor = Color.Black
+        End If
+    End Sub
+End Class
