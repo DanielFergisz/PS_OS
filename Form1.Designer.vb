7e8ceb30abea3197a5712457d6ae8b6ef7232b57
Komputerek PC <Komputerek PC@DESKTOP-KSV668S>
2020-12-17 16:40:24 +0100

    v1.05 - new version

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -544,7 +544,7 @@ Partial Class Form1
         Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "PS_OS v1.04"
+        Me.Text = "PS_OS v1.05"
         Me.TabControl1.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
         Me.TabPage1.PerformLayout()
