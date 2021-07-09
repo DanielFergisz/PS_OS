842d4452f6963445c9151234d6ae97097da00c9b
Komputerek PC <Komputerek PC@DESKTOP-KSV668S>
2021-07-09 13:58:39 +0200

    v1.20 - new version

--- Form1.vb
+++ Form1.vb
@@ -4,7 +4,7 @@ Imports System.Net
 
 Public Class Form1
     Dim appPath As String = IO.Path.Combine(Application.StartupPath, "")
-    Dim appVer As SByte = "119" 'App Version
+    Dim appVer As SByte = "120" 'App Version
 
     '################ Download Address ###############
     Dim PS4_F As String
