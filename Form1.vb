cae703bf1fbb0e767da51da5f327346ba78c6888
Daniel Fergisz <daniel.fergisz@gmail.com>
2021-10-18 07:53:13 +0200

    1.25 - new version

--- Form1.vb
+++ Form1.vb
@@ -4,7 +4,7 @@ Imports System.Net
 
 Public Class Form1
     Dim appPath As String = IO.Path.Combine(Application.StartupPath, "")
-    Dim appVer As SByte = "124" 'App Version
+    Dim appVer As SByte = "125" 'App Version
 
     '################ Download Address ###############
     Dim PS4_F As String
